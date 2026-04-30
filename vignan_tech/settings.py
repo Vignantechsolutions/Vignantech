d0c8890534fb4a80b4a99af598ebc4e30b275d7b
Vignantechsolutions <vignantechsolutions@gmail.com>
2026-04-30 17:43:26 +0530

    fix: switch from SendGrid to Gmail SMTP for OTP emails

--- vignan_tech/settings.py
+++ vignan_tech/settings.py
@@ -110,11 +110,11 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='vignantechsolutions@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Vignan TechSolutions <vignantechsolutions@gmail.com>')
 
 OTP_EXPIRY_MINUTES = 10

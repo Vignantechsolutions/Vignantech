587a7198d14db7b2debda7478d2b9a3fb32bb2ff
Vignantechsolutions <vignantechsolutions@gmail.com>
2026-04-30 13:12:01 +0530

    fix: auto-create admin on render deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin@vignantechsolutions.com').exists():
+    User.objects.create_superuser(username='admin@vignantechsolutions.com', email='admin@vignantechsolutions.com', password='Vignan@2024')
+    print('Admin created')
+else:
+    print('Admin already exists')
+"
